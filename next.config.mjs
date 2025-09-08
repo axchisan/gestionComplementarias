b6c48bf60a16be224e026934703a0b0e39c2c341
axchisan <leftyrancuentabot@gmail.com>
2025-09-07 19:52:46 -0500

    se elimino la ruta health y que estaba causando errores con el estado del contenedor

--- next.config.mjs
+++ next.config.mjs
@@ -48,15 +48,6 @@ const nextConfig = {
       },
     ]
   },
-  
-  async rewrites() {
-    return [
-      {
-        source: '/health',
-        destination: '/api/health',
-      },
-    ]
-  },
 }
 
 export default nextConfig
